8919ed584e8bc91f468aa072d911d4360f5d5bb9
JethlojaUtam <utamjethloja@gmail.com>
2025-02-05 10:46:17 +0530

    Program7.java

--- Program7.java
+++ Program7.java
@@ -11,24 +11,60 @@ public class Program7 {
         int choice;
         Scanner sc=new Scanner(System.in);
         System.out.println("1. Factorial");
-        System.out.println("2. Even or Odd");
+        System.out.println("2. Number is Even or Odd");
         System.out.println("3. Exit");
-        System.out.println("Enter your choice:");
+        System.out.print("Enter your choice: ");
         choice=sc.nextInt();
         switch(choice)
         {
             case 1:
                 int no,i,fact=1;
-                System.out.print("Enter Number:");
+                System.out.print("Enter Number: ");
                 no=sc.nextInt();
                 for(i=1;i<=no;i++)
                 {
                     fact=fact*i;
                 }
-                System.out.println("factorial of "+fact);
+                System.out.println("factorial is "+fact);
                 break;
-            default:
-                System.out.println("Error");
+            
+            case 2:
+                int a;
+                System.out.print("Enter Number: ");
+                a=sc.nextInt();
+                if(a%2==0)
+                {
+                    System.out.println("Number is Even");
+                }    
+                else
+                {
+                    System.out.println("Number is Odd");
+                }
+                break;
+            
+            case 3:
+                System.out.println("Exit");
+                break;
+                default:
+                System.out.println("Invalid choice. Please try again!!!");
         }
     }
 }
+
+//run:
+//1. Factorial
+//2. Number is Even or Odd
+//3. Exit
+//Enter your choice: 1
+//Enter Number: 5
+//factorial is 120
+
+//Enter your choice: 2
+//Enter Number: 1
+//Number is Odd
+
+//Enter your choice: 3
+//Exit
+
+//Enter your choice: 4
+//Invalid choice. Please try again!!!
